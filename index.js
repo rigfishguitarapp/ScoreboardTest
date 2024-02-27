7067bac4da00ec34ec4100c604882d6bc5049254
Tim O’Bryan, the Rigfish Guitar App Guy <62968222+rigfishguitarapp@users.noreply.github.com>
2024-02-27 09:25:14 -0600

    ScoreboardChallenge-TimOB
    This is my completed scoreboard design per the Scrimba Challenge.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 let countH = 0
 let countG = 0
-let homescoreEl = document.getElementById("homescore-el")
-let guestscoreEl = document.getElementById("guestscore-el")
+let hscoreEl = document.getElementById("hscore-el")
+let gscoreEl = document.getElementById("gscore-el")
 
 
 function homeAdd1() {
     countH += 1
-    homescoreEl.textContent = countH 
+    hscoreEl.textContent = countH 
     
 }
 
 function homeAdd2() {
     countH += 2
-    homescoreEl.textContent = countH 
+    hscoreEl.textContent = countH 
 }
 
 function homeAdd3() {
     countH += 3
-    homescoreEl.textContent = countH 
+    hscoreEl.textContent = countH 
 }
 
 function guestAdd1() {
     countG += 1
-    guestscoreEl.textContent = countG 
+    gscoreEl.textContent = countG 
 }
 
 function guestAdd2() {
     countG += 2
-    guestscoreEl.textContent = countG
+    gscoreEl.textContent = countG
 }
 
 function guestAdd3() {
     countG += 3
-    guestscoreEl.textContent = countG 
+    gscoreEl.textContent = countG 
 }
